ceaa8282f2b1ffe93f3b423a0081226854655294
Harel Natan <harelnatan7@gmail.com>
2022-12-05 13:51:08 +0200

    My First Commit

---
+++ js/galley.service.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var gPortfolios = [
+    {
+        id: "minesweeper",
+        name: "Sokoban",
+        title: "The old game we love with new features ",
+        desc: "My first sprint in the Coding Academy Bootcamp",
+        url: "https://harelnatan7.github.io/MinesWeeper/",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "in-picture-game",
+        name: "in-picture-game",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem ipsum lorem ipsum",
+        url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+
+]

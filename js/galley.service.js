25bbec9fd436d3a33c20fbc950c4a5c04ea1254a
Harel Natan <harelnatan7@gmail.com>
2022-12-06 01:28:41 +0200

    My First Official Upload

--- js/galley.service.js
+++ js/galley.service.js
@@ -1,23 +1,64 @@
 'use strict'
 
-var gPortfolios = [
-    {
-        id: "minesweeper",
-        name: "Sokoban",
-        title: "The old game we love with new features ",
-        desc: "My first sprint in the Coding Academy Bootcamp",
-        url: "https://harelnatan7.github.io/MinesWeeper/",
-        publishedAt: 1448693940000,
-        labels: ["Matrixes", "keyboard events"],
-    },
-    {
-        id: "in-picture-game",
-        name: "in-picture-game",
-        title: "Better push those boxes",
-        desc: "lorem ipsum lorem ipsum lorem ipsum",
-        url: "projs/sokoban",
-        publishedAt: 1448693940000,
-        labels: ["Matrixes", "keyboard events"],
-    },
 
+
+var gProjects = [
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        title: 'The old game we love with new features ',
+        desc: 'My first sprint in the Coding Academy Bootcamp',
+        url: 'https://harelnatan7.github.io/MinesWeeper/',
+        publishedAt: 1669586400000,
+        labels: [' Matrixes, Old Games'],
+    },
+    {
+        id: 'in-picture-game',
+        name: 'In Picture Game',
+        title: 'Guess What In The Picture',
+        desc: 'My first ever app, also involvivng what I love the most: Football',
+        url: 'https://harelnatan7.github.io/InPicture/',
+        publishedAt: 1668031200000,
+        labels: [' Football, Guess Me'],
+    },
+    {
+        id: 'book-shop',
+        name: 'Book Shop',
+        title: 'Manage your private Book Shop ',
+        desc: 'Nice project, that was when I first learned CRUDL',
+        url: 'https://harelnatan7.github.io/Book-Shop/',
+        publishedAt: 1670018400000,
+        labels: ['CRUDL, Service, Managment'],
+    },
+    {
+        id: 'ball-board',
+        name: 'Ball Board',
+        title: 'Eat those balls before you die !',
+        desc: 'Fisrt time we used render, also the first board game I built',
+        url: 'https://harelnatan7.github.io/Ball-Board/',
+        publishedAt: 1668463200000,
+        labels: ['Matrixes, keyboard Events, Board Games'],
+    },
+    {
+        id: 'pac-man',
+        name: 'Pac Man',
+        title: 'The old pacman with new features',
+        desc: 'Who doesnt love PacMan? but now, enjoy some extra features',
+        url: 'https://harelnatan7.github.io/Pac-Man/',
+        publishedAt: 1668895200000,
+        labels: ['Matrixes, keyboard Events, Board Games'],
+    },
+    {
+        id: 'todo-app',
+        name: 'To Do app',
+        title: 'Manage your tasks',
+        desc: 'A very useful app to manage your To Do tasks',
+        url: 'https://harelnatan7.github.io/ToDo-App/',
+        publishedAt: 1669845600000,
+        labels: ['CRUDL, JQuery, Tasks'],
+    },
 ]
+
+function getProjects() {
+    return gProjects
+}
